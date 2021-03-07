bbf4990afe93098f32bf44a41ec139860b3c3a5d
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2021-03-07 23:06:31 +0100

    Wrote documentation for AStarAI

--- src/ai/AStarAI.ts
+++ src/ai/AStarAI.ts
@@ -4,14 +4,27 @@ import { Grid, Direction } from '../game/Grid';
 import { findIndex2D, sum2D, factorial, sizeOf, formatNumber, isRunningAsWorker } from '../lib/util';
 import { Status } from '../game/EightPuzzle';
 
+/**
+ * An implementation of the A* algorithm, which searches for a solution to the 8-Puzzle game
+ */
 export class AStarAI implements AI {
-    explored = new StateSet();
+    /**
+    The (priority) queue of states which has not been explored yet.
+    It is sorted asending by the "cost-value" of each game state.
+    **/
     pq = new PriorityQueue<SearchNode>({
         comparator: (a, b) => a.cost() - b.cost(),
     });
+    /**The set of all states which has been explored in the search*/
+    explored = new StateSet();
+    /** The theoretical size of the search space, based on the size of the game board. */
     searchSpace = 0;
+    /** The amount of memory, in bytes, a single grid is consuming. Used for memory usage metrics. */
     gridMemSize = 0;
 
+    /**
+     * Search for a set of moves which transforms the grid into the desired state.
+     */
     getMoves(grid: Grid, desiredState: (number | null)[][]): Direction[] {
         // Calculate constants, for use in progress status
         this.calculateProgressConstants(grid);
@@ -19,10 +32,9 @@ export class AStarAI implements AI {
         // Put initial state on queue
         this.pq.queue(new SearchNode(grid, desiredState));
 
-        // Keep counter to decide when to report status
-        let i = 1;
-
-        while (this.pq.length !== 0) {
+        // Keep running until the queue is empty
+        // Maintain counter to decide when to report status
+        for (let i = 1; this.pq.length !== 0; i++) {
             const node = this.pq.dequeue();
 
             // Goal test
@@ -36,20 +48,25 @@ export class AStarAI implements AI {
                 this.reportProgress();
             }
 
+            // Mark the current node as explored
             this.explored.add(node);
 
+            // For each of valid moves in the current state
             for (const move of node.moves()) {
+                // Make a copy of the current state, in order not to modify it for future iterations
                 const copy = node.copy();
 
+                // Perform the move
                 copy.move(move);
 
+                // If this new configuration is not in our explored set, add it to the queue
                 if (!this.explored.has(copy)) {
                     this.pq.queue(copy);
                 }
             }
-            i++;
         }
 
+        // If we reach this code, no solution was found. Therefore throw error
         const searchedPct = (this.explored.size / this.searchSpace) * 100;
         throw new Error(
             `Could not find a solution.\nExplored ${formatNumber(searchedPct)}% of search space (${formatNumber(
@@ -58,12 +75,18 @@ export class AStarAI implements AI {
         );
     }
 
+    /**
+     * Calculate constants, for use in progress status
+     */
     private calculateProgressConstants(grid: Grid): void {
         // The theoretical bound of search space
         this.searchSpace = factorial(grid.size * grid.size) / 2;
         this.gridMemSize = sizeOf(grid);
     }
 
+    /**
+     * Send progress updates to the main thread, in order to render the progres bar
+     */
     private reportProgress() {
         if (!isRunningAsWorker()) {
             return;
@@ -84,7 +107,7 @@ export class AStarAI implements AI {
 }
 
 /**
- * Simple wrapper around set, which handles comparison between search nodes (thus handling reference equality)
+ * A Simple wrapper around set, which handles comparison between search nodes (thus handling reference equality)
  */
 class StateSet {
     private set = new Set<string>();
@@ -107,6 +130,11 @@ class StateSet {
     }
 }
 
+/**
+ * Represents, and wraps, a concrete game state, and performs
+ * the nessesary calculations (such as heuristics), which is
+ * used during the search.
+ */
 class SearchNode {
     grid: Grid;
     actions: Direction[];
@@ -139,6 +167,9 @@ class SearchNode {
         return this.totalManhattanDistance();
     }
 
+    /**
+     * Return the sum of manhattan distance of all cells in the grid.
+     */
     totalManhattanDistance(): number {
         if (this._manhattanDistanceCache) {
             return this._manhattanDistanceCache;
